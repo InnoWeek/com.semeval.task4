2cf53f67d727669abab8d89628046baffec37758
Svetlin Zarev <svetlin.zarev@sap.com>
2016-11-30 15:47:44 +0200

    Add a 3 layer network example

---
+++ src/main/java/com/semeval/task4/TwoLayerGravesLstmNetworkTrainer.java
@@ -0,0 +1,98 @@
+package com.semeval.task4;
+
+import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
+import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.GradientNormalization;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.layers.GravesLSTM;
+import org.deeplearning4j.nn.conf.layers.RnnOutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public final class TwoLayerGravesLstmNetworkTrainer extends AbstractNetworkTrainer {
+    private TwoLayerGravesLstmNetworkTrainer(Path trainSet, Path testSet, WordVectors wordVectors, int vectorSize) {
+        super(trainSet, testSet, wordVectors, vectorSize);
+    }
+
+    @Override
+    protected MultiLayerNetwork createNetwork() {
+        int nOut = Math.min(vectorSize, 200);
+        final MultiLayerConfiguration configuration = new NeuralNetConfiguration.Builder()
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .iterations(1)
+                .updater(Updater.ADAGRAD)
+                .regularization(true)
+                .l2(1e-5)
+                .weightInit(WeightInit.XAVIER)
+                .gradientNormalization(GradientNormalization.ClipElementWiseAbsoluteValue)
+                .gradientNormalizationThreshold(1.0)
+                .learningRate(0.15)
+                .list()
+                .layer(0, new GravesLSTM.Builder()
+                        .nIn(vectorSize)
+                        .nOut(nOut/2)
+                        .activation("softsign")
+                        .build())
+                .layer(1, new GravesLSTM.Builder()
+                        .nIn(nOut/2)
+                        .nOut(nOut/2)
+                        .activation("tanh")
+                        .build())
+                .layer(2, new RnnOutputLayer.Builder()
+                        .activation("softmax")
+                        .lossFunction(LossFunctions.LossFunction.MCXENT)
+                        .nIn(nOut/2)
+                        .nOut(2)
+                        .build())
+                .pretrain(false)
+                .backprop(true)
+                .build();
+
+        MultiLayerNetwork multiLayerNetwork = new MultiLayerNetwork(configuration);
+        multiLayerNetwork.init();
+        return multiLayerNetwork;
+    }
+
+    public static void main(String[] args) throws TrainingException, IOException {
+        System.out.println("Loading word vectors (" + args[0] + ") ....");
+        System.out.println("Vector size: " + args[1]);
+        final int vectorSize = Integer.parseInt(args[1]);
+        WordVectors wordVectors = WordVectorSerializer.loadStaticModel(new File(args[0]));
+
+        final Path trainSet = Paths.get(args[2]);
+        final Path testSet = Paths.get(args[3]);
+
+        System.out.println("Train set: " + trainSet);
+        System.out.println("Creating trainer...");
+        System.out.println("Test set: " + testSet);
+
+        final TwoLayerGravesLstmNetworkTrainer trainer = new TwoLayerGravesLstmNetworkTrainer(trainSet, testSet, wordVectors, vectorSize);
+        trainer.addPreprocessor(new HashTagPreprocessor());
+        trainer.addPreprocessor(new UrlRemovingPreprocessor());
+        trainer.addPreprocessor(new ReplaceEmoticonsPreprocessor());
+        trainer.addPreprocessor(new PunctuationRemovingPreprocessor());
+        trainer.addPreprocessor(new RepeatingCharsPreprocessor());
+        trainer.addPreprocessor(new ToLowerCasePreprocesor());
+
+        for (int i = 0; i < 12; i++) {
+            trainer.train(1, 20);
+            System.out.println("\nEpoch: " + i);
+            System.out.println(trainer.evaluate(20).stats());
+        }
+
+        try (OutputStream rawOut = Files.newOutputStream(Paths.get("C:\\Users\\i304680\\git\\network.out"))) {
+            trainer.saveNetwork(rawOut);
+        }
+    }
+}
